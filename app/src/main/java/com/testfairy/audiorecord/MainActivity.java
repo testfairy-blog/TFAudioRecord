208544a388cc70d1028f20f86e1f233667553bf2
Diego Perini <diego@dperini.com>
2019-01-21 17:29:34 +0300

    Deleted unused imports

--- app/src/main/java/com/testfairy/audiorecord/MainActivity.java
+++ app/src/main/java/com/testfairy/audiorecord/MainActivity.java
@@ -1,20 +1,9 @@
 package com.testfairy.audiorecord;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.MediaRecorder;
-import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
