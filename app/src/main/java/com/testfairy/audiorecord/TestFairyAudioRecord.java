74085f9902215f7cd4b0e4559ae4bd78db93a979
Diego Perini <diego@dperini.com>
2019-01-22 16:13:44 +0300

    Implemented Wave file creation for recorded samples

--- app/src/main/java/com/testfairy/audiorecord/TestFairyAudioRecord.java
+++ app/src/main/java/com/testfairy/audiorecord/TestFairyAudioRecord.java
@@ -14,7 +14,6 @@ import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.lang.ref.WeakReference;
 import java.util.Date;
 
@@ -28,9 +27,9 @@ public class TestFairyAudioRecord {
 
 	/***************** Audio File Spec *****************/
 
-	static private final int[] RECORDER_SAMPLERATES = new int[] { 11025 };
-	static private final int[] RECORDER_CHANNELS = new int[] { AudioFormat.CHANNEL_IN_MONO };
-	static private final int[] RECORDER_AUDIO_ENCODINGS = new int[] { AudioFormat.ENCODING_PCM_16BIT };
+	static private final int RECORDER_SAMPLERATE = 11025;
+	static private final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+	static private final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT ;
 	static private final int SHORTS_PER_ELEMENT = 2; // 2 bytes in 16bit format, must be in sync with the encoding (above)
 	static private final int IN_MEMORY_FILE_SIZE_IN_BYTES = 500 * 1024;
 	static private final int AUDIO_FILE_MAX_DURATION_IN_SECONDS = 15;
@@ -38,7 +37,7 @@ public class TestFairyAudioRecord {
 
 	/***************** Recorder State *****************/
 
-	WeakReference<Activity> activityWeakReference = new WeakReference<>(null);
+	WeakReference<Activity> activityWeakReference;
 	private AudioRecord recorder = null;
 	private Thread recordingThread = null;
 	private boolean isRecording = false;
@@ -52,7 +51,7 @@ public class TestFairyAudioRecord {
 
 	/***************** Public Interface *****************/
 
-	static public void onCreate(Activity activity) {
+	static public void onCreate(@NonNull Activity activity) {
 		if (instance != null) {
 			synchronized (instance) {
 				instance = new TestFairyAudioRecord(activity);
@@ -60,9 +59,13 @@ public class TestFairyAudioRecord {
 		} else {
 			instance = new TestFairyAudioRecord(activity);
 		}
+
+		Log.d(TAG, "TestFairyAudioRecord initialized.");
 	}
 
 	static public void onResume() {
+		Log.d(TAG, "TestFairyAudioRecord is in foreground.");
+
 		if (instance != null) {
 			synchronized (instance) {
 				instance.startRecording();
@@ -73,6 +76,8 @@ public class TestFairyAudioRecord {
 	}
 
 	static public void onPause() {
+		Log.d(TAG, "TestFairyAudioRecord is in background.");
+
 		if (instance != null) {
 			synchronized (instance) {
 				instance.stopRecording();
@@ -83,6 +88,8 @@ public class TestFairyAudioRecord {
 	}
 
 	static public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		Log.d(TAG, "TestFairyAudioRecord received permission results.");
+
 		if (instance != null) {
 			synchronized (instance) {
 				instance.retryWithRecievedPermissions(requestCode, permissions, grantResults);
@@ -95,7 +102,7 @@ public class TestFairyAudioRecord {
 
 	/***************** Implementation *****************/
 
-	private TestFairyAudioRecord(Activity activity) {
+	private TestFairyAudioRecord(@NonNull Activity activity) {
 		activityWeakReference = new WeakReference<>(activity);
 	}
 
@@ -158,8 +165,11 @@ public class TestFairyAudioRecord {
 
 				if (requestCode == REQUEST_AUDIO_PERMISSION_RESULT) {
 					if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+						Log.d(TAG, "Audio record permission granted.");
 						startRecording();
 					} else {
+						Log.w(TAG, "Audio record permission is denied.");
+
 						Toast.makeText(
 								activity,
 								"Application will not have audio on record",
@@ -203,28 +213,28 @@ public class TestFairyAudioRecord {
 	}
 
 	private AudioRecord findAndCreateAudioRecordFromSpecs() {
-		for (int rate : RECORDER_SAMPLERATES) {
-			for (int audioFormat : RECORDER_AUDIO_ENCODINGS) {
-				for (int channelConfig : RECORDER_CHANNELS) {
-					try {
-						Log.d(TAG, "Attempting rate " + rate + "Hz, bits: " + audioFormat + ", channel: "
-								+ channelConfig);
+		try {
+			Log.d(TAG, "Attempting rate " + RECORDER_SAMPLERATE + "Hz, bits: " + RECORDER_AUDIO_ENCODING + ", channel: "
+					+ RECORDER_CHANNELS);
 
-						bufferElements2Rec = AudioRecord.getMinBufferSize(rate, channelConfig, audioFormat);
+			bufferElements2Rec = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING);
 
-						if (bufferElements2Rec != AudioRecord.ERROR_BAD_VALUE) {
-							// check if we can instantiate and have a success
-							AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, rate, channelConfig, audioFormat, bufferElements2Rec);
+			if (bufferElements2Rec != AudioRecord.ERROR_BAD_VALUE) {
+				// check if we can instantiate and have a success
+				AudioRecord recorder = new AudioRecord(
+						MediaRecorder.AudioSource.MIC,
+						RECORDER_SAMPLERATE,
+						RECORDER_CHANNELS,
+						RECORDER_AUDIO_ENCODING,
+						bufferElements2Rec
+				);
 
-							if (recorder.getState() == AudioRecord.STATE_INITIALIZED)
-								return recorder;
-							Log.d(TAG, "Failed to init: " + recorder.getState());
-						}
-					} catch (Throwable e) {
-						Log.e(TAG, rate + "Exception, keep trying.",e);
-					}
-				}
+				if (recorder.getState() == AudioRecord.STATE_INITIALIZED)
+					return recorder;
+				Log.d(TAG, "Failed to init: " + recorder.getState());
 			}
+		} catch (Throwable e) {
+			Log.e(TAG, RECORDER_SAMPLERATE + "Exception, keep trying.",e);
 		}
 
 		return null;
@@ -262,7 +272,7 @@ public class TestFairyAudioRecord {
 							recorder.read(soundBuffer, 0, SHORTS_PER_ELEMENT);
 
 							if (logStopWatch.stopIfAboveTimeLimit(1)) {
-								Log.d(TAG, "Short wirting to file" + soundBuffer.toString());
+								Log.d(TAG, "Audio is being written to memory file" + soundBuffer.toString());
 								logStopWatch.start();
 							}
 
@@ -292,9 +302,18 @@ public class TestFairyAudioRecord {
 		}
 	}
 
-	private void flushOutputStreamToTestFairy(ByteArrayOutputStream os) {
-		// TODO
+	private void flushOutputStreamToTestFairy(@NonNull ByteArrayOutputStream os) {
 		Log.d(TAG, "Flushing audio to TestFairy");
+
+		try {
+			AudioSample sample = new AudioSample(RECORDER_SAMPLERATE, SHORTS_PER_ELEMENT * 8, 1);
+			byte[] wavFile = sample.toWavFile(os.toByteArray());
+
+			// TODO : send wavFile to TestFairy
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 
@@ -345,6 +364,81 @@ public class TestFairyAudioRecord {
 			} else {
 				return false;
 			}
+		}
+	}
+
+	public static class AudioSample {
+
+		// Header template to copy into each Wave file, will be edited after copy.
+		static private final byte[] header = {
+				'R', 'I', 'F', 'F',
+				'0', '0', '0', '0',     // chunk size
+				'W', 'A', 'V', 'E',
+				'f', 'm', 't', ' ',
+				0x10, 0x00, 0x00, 0x00, // subchunk size (16 bytes)
+				0x01, 0x00,             // audio format pcm
+				0x01, 0x00,             // total channels
+				0x00, 0x00, 0x00, 0x00, // sample rate
+				0x00, 0x00, 0x00, 0x00, // byte rate
+				0x04, 0x00,             // block align
+				0x10, 0x00,             // bits per sample
+				'd', 'a', 't', 'a',
+				0x00, 0x00, 0x00, 0x00, // total sample data size
+		};
+
+		private int sampleRate;
+		private int bitsPerSample;
+		private int channels;
+
+		public AudioSample(int sampleRate, int bitsPerSample, int channels) {
+			this.sampleRate = sampleRate;
+			this.bitsPerSample = bitsPerSample;
+			this.channels = channels;
+		}
+
+		/**
+		 * Join all audio chunks into one playable RIFF wave file
+		 *
+		 * @return byte[]
+		 * @throws IOException
+		 */
+		public byte[] toWavFile(@NonNull byte[] audioData) throws IOException {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+			baos.write(header);
+			baos.write(audioData, 0, audioData.length);
+
+			byte[] out = baos.toByteArray();
+
+			int byteRate = (sampleRate * bitsPerSample * channels) / 8;
+			int fileSize = out.length + header.length - 4;
+
+			out[0x04] = (byte) (fileSize & 0xff);
+			out[0x05] = (byte) ((fileSize >> 8) & 0xff);
+			out[0x06] = (byte) ((fileSize >> 16) & 0xff);
+			out[0x07] = (byte) ((fileSize >> 24) & 0xff);
+
+			out[0x16] = (byte) (channels & 0xff);
+
+			out[0x18] = (byte) (sampleRate & 0xff);
+			out[0x19] = (byte) ((sampleRate >> 8) & 0xff);
+			out[0x1a] = (byte) ((sampleRate >> 16) & 0xff);
+			out[0x1b] = (byte) ((sampleRate >> 24) & 0xff);
+
+			out[0x1c] = (byte) (byteRate & 0xff);
+			out[0x1d] = (byte) ((byteRate >> 8) & 0xff);
+			out[0x1e] = (byte) ((byteRate >> 16) & 0xff);
+			out[0x1f] = (byte) ((byteRate >> 24) & 0xff);
+
+			out[0x22] = (byte) (bitsPerSample & 0xff);
+			out[0x23] = (byte) ((bitsPerSample >> 8) & 0xff);
+
+			out[0x28] = (byte) (out.length & 0xff);
+			out[0x29] = (byte) ((out.length >> 8) & 0xff);
+			out[0x2a] = (byte) ((out.length >> 16) & 0xff);
+			out[0x2b] = (byte) ((out.length >> 24) & 0xff);
+
+			return out;
 		}
 	}
 
