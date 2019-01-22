b808c3a9590612236af8fc3761d6c5c153089033
Diego Perini <diego@dperini.com>
2019-01-22 16:25:49 +0300

    Added SDK to gradle and MainApplication

---
+++ app/src/main/java/com/testfairy/audiorecord/MainApplication.java
@@ -0,0 +1,17 @@
+package com.testfairy.audiorecord;
+
+import android.app.Application;
+
+import com.testfairy.TestFairy;
+
+public class MainApplication extends Application {
+
+	static public final String APP_TOKEN = "";
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		TestFairy.begin(this, APP_TOKEN);
+	}
+}
